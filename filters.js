e20e5d48364151f3699a00a10f07bffae4e1a072
lemilleur <49449952+lemilleur@users.noreply.github.com>
2024-05-02 22:41:01 +0300

    Update filters.js

--- filters.js
+++ filters.js
@@ -6,9 +6,15 @@ document.addEventListener('DOMContentLoaded', (event) => {
     if (toggleButton) {
       toggleButton.addEventListener('click', function() {
         filters.classList.toggle('active');
+        // Изменяем текст кнопки в зависимости от состояния фильтров
+        if (filters.classList.contains('active')) {
+          toggleButton.textContent = 'Скрыть фильтры';
+        } else {
+          toggleButton.textContent = 'Показать фильтры';
+        }
       });
     } else {
       console.error('Toggle button not found');
     }
-  }, 1000); // Задержка в 1 сек
+  }, 1000); // Задержка в 1 секунду
 });
