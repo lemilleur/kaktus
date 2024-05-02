ee623d5794e5dd70b576a0bc91f13d4ab053349b
lemilleur <49449952+lemilleur@users.noreply.github.com>
2024-05-02 22:32:27 +0300

    Update filters.js

--- filters.js
+++ filters.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  var toggleButton = document.getElementById('toggleFilters');
-  var filters = document.querySelector('.cactus-category__filter');
+  setTimeout(function() {
+    var toggleButton = document.getElementById('toggleFilters');
+    var filters = document.querySelector('.cactus-category__filter');
 
-  if (toggleButton) {
-    toggleButton.addEventListener('click', function() {
-      filters.classList.toggle('active');
-    });
-  } else {
-    console.error('Toggle button not found');
-  }
+    if (toggleButton) {
+      toggleButton.addEventListener('click', function() {
+        filters.classList.toggle('active');
+      });
+    } else {
+      console.error('Toggle button not found');
+    }
+  }, 100); // Задержка в 100 миллисекунд
 });
